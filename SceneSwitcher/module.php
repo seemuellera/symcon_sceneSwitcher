b68b9358ee047b97b1e3e8f67be9114362670d85
Andreas Seemueller <andreas.seemueller@gmail.com>
2023-08-03 21:55:51 +0200

    VM_UPDATE

--- SceneSwitcher/module.php
+++ SceneSwitcher/module.php
@@ -89,7 +89,7 @@ class SceneSwitcher extends IPSModule {
 		$messagesList = $this->GetMessageList();
 		foreach ($messagesList as $currentMessage) {
 
-			$this->UnregisterMessage($currentMessage, VM_CHANGE);
+			$this->UnregisterMessage($currentMessage, VM_UPDATE);
 		}
 
 		$this->RegisterReference($this->ReadPropertyInteger("TargetStatusVariableId"));
